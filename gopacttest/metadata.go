30dd50542c639d5c83595de3e5bd1a3e424757a8
LPX-E5BD8 <lipengxiang_@outlook.com>
2026-06-28 15:51:36 +0800

    fix: expose review failure metadata keys

--- gopacttest/metadata.go
+++ gopacttest/metadata.go
@@ -1,5 +1,7 @@
 package gopacttest
 
+import "sort"
+
 func mergeSupplementalMetadata(metadata map[string]any, supplemental map[string]any, reserved func(string) bool) {
 	for key, value := range supplemental {
 		if reserved != nil && reserved(key) {
@@ -7,4 +9,22 @@ func mergeSupplementalMetadata(metadata map[string]any, supplemental map[string]
 		}
 		metadata[key] = value
 	}
+}
+
+func sortedSupplementalMetadataKeys(supplemental map[string]any, reserved func(string) bool) []string {
+	if len(supplemental) == 0 {
+		return nil
+	}
+	keys := make([]string, 0, len(supplemental))
+	for key := range supplemental {
+		if reserved != nil && reserved(key) {
+			continue
+		}
+		keys = append(keys, key)
+	}
+	if len(keys) == 0 {
+		return nil
+	}
+	sort.Strings(keys)
+	return keys
 }
